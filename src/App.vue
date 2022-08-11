17dc9c87d1950e42622e0b4a52c22a5f35f4396c
Alwayfeels <547542652@qq.com>
2022-08-11 09:42:44 +0800

    feat: create repo for ts

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,85 @@
-<template>
-  <n-notification-provider>
-    <div id="app">
-      <TopBar class="fixed shadow z-10" />
-      <router-view class="mt-20" />
-    </div>
-  </n-notification-provider>
-</template>
-
-<script setup>
-import TopBar from '@/components/TopBar.vue';
-import { useGlobalData } from './store/globalData';
-
-const globalData = useGlobalData()
-globalData.init()
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style>
-#app {
-  overflow: hidden;
-  font-family: "Inter", "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
-  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
 }
 
-html {
-  overflow-y: scroll;
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
 }
 
-.mini-tag {
-  height: 16px;
-  padding: 0 4px;
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
 }
 
-/* 滚动槽 */
-/* ::-webkit-scrollbar {
-  width: 6px;
-  height: 6px;
+nav a.router-link-exact-active {
+  color: var(--color-text);
 }
 
-::-webkit-scrollbar-track {
-  border-radius: 3px;
-  background: rgba(0, 0, 0, 0.06);
-  -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.08);
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
 }
 
-::-webkit-scrollbar-thumb {
-  border-radius: 3px;
-  background: rgba(0, 0, 0, 0.12);
-  -webkit-box-shadow: inset 0 0 10px rgba(0, 0, 0, 0.2);
-} */
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
 </style>
