08d404c0b72b98e76855de466ff686a0def9ec7f
Alwayfeels <547542652@qq.com>
2022-10-11 23:18:53 +0800

    feat: update demo animation

--- src/components/DemoAnimate.vue
+++ src/components/DemoAnimate.vue
@@ -1,13 +1,79 @@
 <template>
-    <div>
-        
-    </div>
+  <div class="scroll-wrapper">
+    <Vue3SeamlessScroll :list="list" direction="right" hover>
+      <!-- <template v-for="item in list" :key="item">
+        <div class="px-6 py-2 mx-4 my-2 scroll-btn">{{item}}</div>
+      </template> -->
+      <div class="px-6 py-2 mx-4 my-2 scroll-btn">测试</div>
+    </Vue3SeamlessScroll>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { NButton, NIcon, NTag } from "naive-ui";
+import { h, Transition } from "vue";
+import { Vue3SeamlessScroll } from "vue3-seamless-scroll";
 
+function getTagButton(tag: string) {
+  return h(
+    NButton,
+    {
+      class: "ml-4 px-6",
+      type: "success",
+      size: "large",
+      strong: true,
+    },
+    tag
+  );
+}
+
+const list = Array(20)
+  .fill(null)
+  .map((e, index) => `测试 ${index}`);
+// const render = () => {
+//   return h(
+//     Vue3SeamlessScroll,
+//     {
+//       list: list,
+//       direction: "right",
+//       hover: true,
+//       class: "pt-4 w-full bg-gray-100 scroll-wrapper",
+//     },
+//     () => {
+//       return list.map(e => getTagButton(e));
+//     }
+//   );
+// };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.button-moving {
+  animation: moveRightAnimation 6s linear;
+}
 
+.scroll-wrapper {
+  overflow: hidden;
+  height: 100px;
+  width: 100%;
+}
+
+.scroll-btn {
+  background-color: #36ad6a;
+  border-radius: 4px;
+  text-align: center;
+  color: #fff;
+  cursor: pointer;
+}
+.scroll-btn:hover {
+  background: #0c7a43;
+}
+
+@keyframes moveRightAnimation {
+  from {
+    transform: translateX(0px);
+  }
+  to {
+    transform: translateX(300px);
+  }
+}
 </style>
--- src/views/IntroView.vue
+++ src/views/IntroView.vue
@@ -1,32 +1,30 @@
 <template>
   <div class="view-container flex flex-col items-center w-full ml-10">
-    <div class="screen-view items-center pt-20">
-      <div class="flex">
-        <div class="flex flex-col">
-          <h1 class="text-5xl mt-12">Tagging Music</h1>
-          <p class="text-2xl text-slate-400 test-class">使用 Tag 标记歌曲，然后快速生成歌单</p>
-          <div class="flex ml-8 my-16">
-            <NButton class="my-4" type="primary" secondary size="large" @click="toIntro">
-              <span>使用方法</span>
-              <n-icon class="ml-2" :size="24" :component="ArrowCircleDown24Filled" />
-            </NButton>
-            <NButton class="ml-4 my-4" type="primary" size="large" @click="toMainView">
-              <span>开始使用</span>
-              <n-icon class="ml-2" :size="20" :component="ArrowCircleRight24Filled" />
-            </NButton>
-          </div>
-        </div>
-        <div class="flex-1">
-          <DemoAnimate />
+    <div class="screen-view flex items-center pt-20">
+      <div class="flex shrink-0 mr-8 flex-col">
+        <h1 class="text-5xl mt-12">Tagging Music</h1>
+        <p class="text-2xl text-slate-400 test-class">使用 Tag 标记歌曲，然后快速生成歌单</p>
+        <div class="flex ml-8 my-16">
+          <NButton class="my-4" type="primary" secondary size="large" @click="toIntro">
+            <span>使用方法</span>
+            <n-icon class="ml-2" :size="24" :component="ArrowCircleDown24Filled" />
+          </NButton>
+          <NButton class="ml-4 my-4" type="primary" size="large" @click="toMainView">
+            <span>开始使用</span>
+            <n-icon class="ml-2" :size="20" :component="ArrowCircleRight24Filled" />
+          </NButton>
         </div>
       </div>
-      <n-divider />
-      <div class="detail flex flex-1">
-        <div v-for="(item, index) in detailInfo" :key="index" class="flex-1 px-4">
-          <div class="title text-xl">{{ item.title }}</div>
-          <div class="content mt-4 text-slate-500">
-            <p v-for="row in item.content" :key="row">{{ row }}</p>
-          </div>
+      <div class="scroll-wrapper">
+        <DemoAnimate />
+      </div>
+    </div>
+    <n-divider />
+    <div class="detail flex flex-1">
+      <div v-for="(item, index) in detailInfo" :key="index" class="flex-1 px-4">
+        <div class="title text-xl">{{ item.title }}</div>
+        <div class="content mt-4 text-slate-500">
+          <p v-for="row in item.content" :key="row">{{ row }}</p>
         </div>
       </div>
     </div>
@@ -39,45 +37,48 @@
 
 <script setup lang="ts">
 import { NButton, NDivider, NIcon } from "naive-ui";
-import { ref, getCurrentInstance } from 'vue';
-import { useRouter } from 'vue-router'
-import DemoAnimate from '@/components/DemoAnimate.vue'
-import { ArrowCircleDown24Filled, ArrowCircleRight24Filled } from '@vicons/fluent'
+import { ref, getCurrentInstance } from "vue";
+import { useRouter } from "vue-router";
+import DemoAnimate from "@/components/DemoAnimate.vue";
+import {
+  ArrowCircleDown24Filled,
+  ArrowCircleRight24Filled,
+} from "@vicons/fluent";
 
-const app = getCurrentInstance()
-const router = useRouter()
+const app = getCurrentInstance();
+const router = useRouter();
 
 const detailInfo = ref([
   {
-    title: '技术栈',
+    title: "技术栈",
     content: [
-      'Tagging Music 基于 NeteaseCloudMusicApi 开发（目前只支持网易云），使用 indexedDB 缓存数据（新增云端存储）。',
-      '清除浏览器缓存将会丢失所有Tag数据，清除缓存前请记得保存数据避免数据丢失。',
-      '技术栈(前端)：Vue3(compositionApi) + Typescript + Vite + Pinia + NaiveUI + TailwindCSS',
-      '技术栈(其他)：express, mongoDB, Localforage, Cypress'
-    ]
+      "Tagging Music 基于 NeteaseCloudMusicApi 开发（目前只支持网易云），使用 indexedDB 缓存数据（新增云端存储）。",
+      "清除浏览器缓存将会丢失所有Tag数据，清除缓存前请记得保存数据避免数据丢失。",
+      "技术栈(前端)：Vue3(compositionApi) + Typescript + Vite + Pinia + NaiveUI + TailwindCSS",
+      "技术栈(其他)：express, mongoDB, Localforage, Cypress",
+    ],
   },
   {
-    title: '开发原因',
+    title: "开发原因",
     content: [
-      '网易云音乐的歌单快速导入是基于古老的多选框构建的',
-      '如果想要把歌曲分类成不同的维度来创建歌单：比如『粤语/男/情歌』，分类会十分麻烦。并且如果再想创建『粤语/女/情歌』那么所有的工作必须从头开始。',
-      'TaggingMusic 灵感来自 flomo, 使用可重复利用的标签来分类歌曲，快速创建歌单。',
-      '本项目仅供本人学习讨论使用，不保证后续的维护和开发'
-    ]
-  }
-])
+      "网易云音乐的歌单快速导入是基于古老的多选框构建的",
+      "如果想要把歌曲分类成不同的维度来创建歌单：比如『粤语/男/情歌』，分类会十分麻烦。并且如果再想创建『粤语/女/情歌』那么所有的工作必须从头开始。",
+      "TaggingMusic 灵感来自 flomo, 使用可重复利用的标签来分类歌曲，快速创建歌单。",
+      "本项目仅供本人学习讨论使用，不保证后续的维护和开发",
+    ],
+  },
+]);
 
 function toMainView() {
-  router.push('/home')
+  router.push("/home");
 }
 
 function toIntro() {
   (app as any).proxy.$notification.create({
-    type: 'error',
+    type: "error",
     title: "在做了在做了!",
-    duration: 3000
-  })
+    duration: 3000,
+  });
 }
 </script>
 
@@ -89,10 +90,23 @@ function toIntro() {
   min-width: 800px;
 
   .screen-view {
+    width: 100%;
     height: 100%;
     overflow: hidden;
   }
 
-  .footer {}
+  .scroll-wrapper {
+    height: 100%;
+    min-width: 500px;
+  }
+  .footer {
+  }
+}
+
+@media screen and (max-width: 1280px) {
+  .view-container {
+    min-width: 800px;
+    padding: 0 20px;
+  }
 }
 </style>
